eb7f7640c1430af91ed47e764a809609aa9d32d0
agmorrow <morrow.austin5@gmail.com>
2022-03-30 20:36:11 -0600

    execute association methods

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -5,8 +5,17 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
+  // define columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      PrimaryKey: true,
+      autoIncrement: true,
+    },
+    tag: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
